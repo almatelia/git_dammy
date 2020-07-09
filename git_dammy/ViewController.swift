c8a0add56330fb5d3f8eea6d6530f6317b61efb2
Toda <kamisamahiziwaru@gmail.com>
2020-07-09 23:18:27 +0900

    first_set

---
+++ git_dammy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_dammy
+//
+//  Created by 戸田優貴 on 2020/07/09.
+//  Copyright © 2020 戸田優貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
